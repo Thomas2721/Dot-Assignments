38e1e339ea37699df963c0e66e682b6238fe488c
Thomas2721 <thomasmehari2721@gmail.com>
2024-11-26 04:53:33 -0800

    Add files via upload

---
+++ Assign 1/index.js
@@ -0,0 +1,27 @@
+ 
+ 
+  // Q1
+
+const add= (a,b) => a + b ;
+console.log(add(5,5));
+
+//Q2
+
+
+const mltiply= (a,b) => a * b ;
+console.log(mltiply(5,5));
+
+
+//Q3
+
+
+const greet = name => 'Hello, ' + name;
+console.log(greet('Thomas'));
+
+
+
+
+
+
+
+
